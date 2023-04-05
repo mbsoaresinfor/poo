c6375c2db7562568d23d0b73d8ba3652fb54e51f
Marcelo Soares <marcelo@rodosoft.com.br>
2023-04-05 10:13:50 -0300

    inicioo projeto

---
+++ POO-1/POO-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POO_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
