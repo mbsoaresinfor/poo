134d643932c7573a837e00b58fd85173c26da2cc
Marcelo Soares <marcelo@rodosoft.com.br>
2023-04-26 10:37:58 -0300

    aula 3

--- POO-2/POO-2/Program.cs
+++ POO-2/POO-2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using POO_2.entidades;
+using System;
 
 namespace POO_2
 {
@@ -6,7 +7,29 @@ namespace POO_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cachorro dog1 = new Cachorro();
+            Console.WriteLine(dog1.nome);
+            dog1.nome = "pity";
+            Console.WriteLine(dog1.nome);
+            dog1.peso = 5;
+            dog1.late();
+
+            Cachorro dog2 = new Cachorro();
+            dog2.nome = "luck";
+            dog2.late();
+
+            if (dog2.Equals(dog1))
+            {
+                Console.WriteLine("os dogs são iguais");
+            }
+            else
+            {
+                Console.WriteLine("os dogs não são iguais");
+            }
+
+            Console.WriteLine(dog1 +  " racao é " + dog1.racao);
+            dog1.peso.ToString();
+            Console.WriteLine(dog2);
         }
     }
 }
---
+++ POO-2/POO-2/entidades/Cachorro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_2.entidades
+{
+    public class Cachorro
+    {
+        public String nome { get; set; }
+        public String racao { get; set; }
+        public long peso { get; set; }
+
+        public void late()
+        {
+            Console.WriteLine("o dog " + nome + " está latindo");
+        }
+
+        public void comer()
+        {
+            Console.WriteLine("o dog " + nome + " está comendo");
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Cachorro cachorro &&
+                   nome == cachorro.nome;
+        }
+
+        public override string ToString()
+        {
+            return $"O nome do dog {nome} tem {peso} kilos";
+        }
+    }
+}
