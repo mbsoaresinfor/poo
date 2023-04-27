2f8d6c2900dfec12051efd1cacb94d8f1a60b943
Marcelo Soares <marcelo@rodosoft.com.br>
2023-04-27 11:06:52 -0300

    aula 3

--- POO-3/POO-3/Program.cs
+++ POO-3/POO-3/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using POO_3.dominios;
+using System;
+using System.Collections.Generic;
 
 namespace POO_3
 {
@@ -6,9 +8,50 @@ namespace POO_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(fatorial(2));
-            
-            
+            Cachorro c1 = new Cachorro();
+            c1.nome = "luck";
+            c1.comer();
+
+            Cachorro c2 = new Cachorro();
+            c2.nome = "pity";
+            c2.comer();
+
+            List<Cachorro> listaDogbancoDados = new List<Cachorro>();
+            listaDogbancoDados.Add(c1);
+            listaDogbancoDados.Add(c2);
+            String nomeDog = "luck";
+
+            foreach(Cachorro cachorro in listaDogbancoDados)
+            {
+                if(cachorro.nome == nomeDog)
+                {
+                    Console.WriteLine("achei o cachorro 1 ");
+                }
+            }
+
+            Cachorro cachorroParaBuscar = new Cachorro();
+            cachorroParaBuscar.nome = "luck";
+
+            if (listaDogbancoDados.Contains(cachorroParaBuscar))
+            {
+                Console.WriteLine("achei o cachorro 2");
+            }
+
+            if (c1.Equals(c2))
+            {
+                Console.WriteLine("os cachorros são iguais");
+            }
+            else
+            {
+                Console.WriteLine("os cachorros não são iguais");
+            }
+
+            Console.WriteLine($"o nome do cachrro e {c1.nome} e sua racao {c1.racao}");
+            Console.WriteLine($"o nome do cachrro e {c2.nome} e sua racao {c2.racao}");
+            c1.nome = "";
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+
         }
         public static int fatorial(int n)
         {
---
+++ POO-3/POO-3/dominios/Cachorro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_3.dominios
+{
+    public class Cachorro
+    {
+        public String nome { get; set; }
+        public String racao { get; set; }
+        public float peso { get; set; }
+
+        public void comer()
+        {
+            Console.WriteLine($"O cachorro {nome} está comendo");
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Cachorro cachorro &&
+                   nome == cachorro.nome;
+        }
+
+        public override string ToString()
+        {
+            return $"o nome do cachrro e {nome} e sua racao {racao}";
+        }
+    }
+}
