029e6a5a192601180e0c86c85bd6686ceecfc450
Marcelo Soares <marcelo@rodosoft.com.br>
2023-06-22 11:39:00 -0300

    aula 11

--- POO-3/POO-3/controller/PessoaController.cs
+++ POO-3/POO-3/controller/PessoaController.cs
@@ -9,12 +9,42 @@ namespace POO_3.controller
 {
     class PessoaController
     {
+
+        PessoaController() { }
+
+        PessoaController(string nome) { }
+
+        PessoaController(int idade) { }
+
+        PessoaController(int idade,string nome) { }
+
+
         PessoaServico pessoaService;
         
         public void salvaPessoa(Pessoa pessoa)
         {
             // transfo. pessoa -> pessoaEntidade.
             pessoaService.salvar(null);
+        }
+
+        public int salvaPessoa(string nome, int idade)
+        {
+            // transfo. pessoa -> pessoaEntidade.
+            pessoaService.salvar(null);
+            return -1;
+        }
+
+        public int salvaPessoa(int idade,string nome)
+        {
+            // transfo. pessoa -> pessoaEntidade.
+            pessoaService.salvar(null);
+            return -1;
+        }
+
+        public void salvaPessoa(int idade, string nome,double peso)
+        {
+            // transfo. pessoa -> pessoaEntidade.
+            pessoaService.salvar(null);         
         }
     }
 }
